2f402996f75fa1fe4668976b751f8691a2060e70
Thomas Beckmann (M-Way) <t.beckmann@mwaysolutions.com>
2016-06-28 12:09:31 +0200

    further tests

---
+++ src/core/mocha.spec.ts
@@ -0,0 +1,45 @@
+/**
+ * @file core/mocha.spec.ts
+ * Relution SDK
+ *
+ * Created by Thomas Beckmann on 28.06.2016
+ * Copyright 2016 M-Way Solutions GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert, expect} from 'chai';
+
+describe(module.filename, () => {
+  return [
+
+    it('assert samples', () => {
+      var x = {};
+      // assert.valueOf(x, 'object', 'x is an object.');
+      assert.isObject(x, 'x is an object.');
+      assert.ok(1 !== 2, '1 is not 2');
+      assert.equal(-1, [1, 2, 3].indexOf(5));
+      assert.equal(-1, [1, 2, 3].indexOf(0));
+    }),
+
+    it('expect samples', () => {
+      var foo = 'bar'
+      var beverages = {tea: ['chai', 'matcha', 'oolong']};
+      expect(foo).to.be.a('string');
+      expect(foo).to.equal('bar');
+      expect(foo).to.have.length(3);
+      expect(beverages).to.have.property('tea').with.length(3);
+    })
+
+  ];
+});
---
+++ src/core/q.spec.ts
@@ -0,0 +1,55 @@
+/**
+ * @file core/q.spec.ts
+ * Relution SDK
+ *
+ * Created by Thomas Beckmann on 28.06.2016
+ * Copyright 2016 M-Way Solutions GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert} from 'chai';
+
+import * as Q from 'q';
+
+describe(module.filename, () => {
+  return [
+
+    it('Q direct resolve', (done) => {
+      Q.resolve(1).then((x) => {
+        assert.equal(x, 1, 'one');
+      }).then(done, done).done();
+    }),
+
+    it('Q indirect resolve', (done) => {
+      Q.fcall(() => {
+        return 1;
+      }).then((x) => {
+        assert.equal(x, 1, 'one');
+      }).then(done, done).done();
+    }),
+
+    it('Q empty resolve', (done) => {
+      Q.resolve(undefined).then((x) => {
+        assert.equal(x, undefined, 'undefined');
+      }).then(done, done).done();
+    }),
+
+    it('Q empty resolve all', (done) => {
+      Q.all([]).then((x) => {
+        assert.deepEqual(x, [], 'undefined');
+      }).then(done, done).done();
+    })
+
+  ];
+});
