2bac855b4fef806c9c29dd1934849a74d46487ea
DrMabuse23 <p.brewing@mwaysolutions.com>
2016-07-26 17:10:04 +0200

    MDM-11596 add postinstall message for the DefinelyType

---
+++ bin/installInfo.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+'use strict';
+console.log('##########################################');
+console.log('# Please add the following typings manually:');
+console.log('# typings install dt~es6-collections dt~es6-promise dt~backbone dt~cordova/plugins/device dt~phonegap-plugin-push dt~websql dt~backbone-global dt~q dt~jquery dt~underscore dt~node --save --global');
+console.log('# typings install npm~lodash --save')
+console.log('# Thank you, the Relution Team.');
+console.log('##########################################');
