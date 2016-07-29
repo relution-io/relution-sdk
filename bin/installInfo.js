aa5dec4998b108baf462c3b6b613aab756c1f66b
DrMabuse23 <p.brewing@mwaysolutions.com>
2016-07-29 09:56:25 +0200

    rm dt~es6-collections dt~es6-promise

--- bin/installInfo.js
+++ bin/installInfo.js
@@ -2,7 +2,7 @@
 'use strict';
 console.log('##########################################');
 console.log('# Please add the following typings manually:');
-console.log('# typings install dt~es6-collections dt~es6-promise dt~backbone dt~cordova/plugins/device dt~phonegap-plugin-push dt~websql dt~backbone-global dt~q dt~jquery dt~underscore dt~node --save --global');
+console.log('# typings install dt~backbone dt~cordova/plugins/device dt~phonegap-plugin-push dt~websql dt~backbone-global dt~q dt~jquery dt~underscore dt~node --save --global');
 console.log('# typings install npm~lodash --save')
 console.log('# Thank you, the Relution Team.');
 console.log('##########################################');
